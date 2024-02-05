81677329bca4eb7a4b0770e274ea9d60779059d3
ArbonShehu <arbonshehu@gmail.com>
2024-02-05 17:15:44 +0100

    Created Carpet Class and Carpet Clients

---
+++ src/day_11_custom_class_part2/CarpetClients.java
@@ -0,0 +1,14 @@
+package day_11_custom_class_part2;
+
+public class CarpetClients {
+
+    public static void main(String[] args) {
+
+        Carpet carpet1 = new Carpet(2,2,10,true);
+        System.out.println(carpet1);
+
+        Carpet carpet2 = new Carpet(2.5,3,10,false);
+        System.out.println(carpet2);
+
+    }
+}
