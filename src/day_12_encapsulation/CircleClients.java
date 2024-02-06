921246526288761a5fa9d6ee0f6ec1902e4f4817
ArbonShehu <arbonshehu@gmail.com>
2024-02-06 16:31:05 +0100

    Created Cirlce Class and Circle Clients

---
+++ src/day_12_encapsulation/CircleClients.java
@@ -0,0 +1,25 @@
+package day_12_encapsulation;
+
+public class CircleClients {
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle(2);
+
+        System.out.println(circle1);
+
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        Circle circle2 = new Circle(3.5);
+        System.out.println(circle2);
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        Circle circle3 = new Circle(-1);
+        System.out.println(circle3);
+
+
+
+    }
+
+}
