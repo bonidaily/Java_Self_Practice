ecd6463da7f1daf9c2ce8c47872e0cf1e5ec84ec
ArbonShehu <arbonshehu@gmail.com>
2024-02-09 13:15:46 +0100

    added for each loop to the task

--- src/day_10_wrapper_class_arraysList/MultipOddNumbers.java
+++ src/day_10_wrapper_class_arraysList/MultipOddNumbers.java
@@ -10,15 +10,25 @@ public class MultipOddNumbers {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 
 
-        for (int i = 0; i < list.size(); i++) {
-            int num = list.get(i);
-            if(num % 2 != 0){
-            list.set(i, num*2);// here we set the num*2 at every i .... set(index, object)
+//        for (int i = 0; i < list.size(); i++) {
+//            int num = list.get(i);
+//            if(num % 2 != 0){
+//            list.set(i, num*2);// here we set the num*2 at every i .... set(index, object)
+//            }
+//        }
+//        System.out.println(list);
+
+        for (Integer element : list) {
+            if(element % 2 !=0){
+                list.set(list.indexOf(element), element*2 );
             }
         }
         System.out.println(list);
     }
 }
+
+
+
 /*
 
 6. Create a class called MultipOddNumbers and write a program with the following specifications:
